8ddae47ac3cedee6153285fa599c34d669d1bd8c
Srikant Sarwa <ssarwa@Srikants-MBP.guest.corp.microsoft.com>
2020-01-16 15:04:37 -0800

    update values

---
+++ src/main/java/security/WebSecurityConfiguration.java
@@ -0,0 +1,27 @@
+package azuresimplainspringboot.Simplaindemo.security;
+
+import com.microsoft.azure.spring.autoconfigure.b2c.AADB2COidcLoginConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final AADB2COidcLoginConfigurer configurer;
+
+    public WebSecurityConfiguration(AADB2COidcLoginConfigurer configurer) {
+        this.configurer = configurer;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .apply(configurer)
+        ;
+    }
+}
