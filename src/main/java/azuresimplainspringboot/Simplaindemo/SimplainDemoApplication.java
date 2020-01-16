26288e8c8e5dcaab4287cf1f94107bf1c07b7dc9
Srikant Sarwa <ssarwa@Srikants-MacBook-Pro.local>
2020-01-16 09:41:08 -0800

    initial code

---
+++ src/main/java/azuresimplainspringboot/Simplaindemo/SimplainDemoApplication.java
@@ -0,0 +1,13 @@
+package azuresimplainspringboot.Simplaindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplainDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplainDemoApplication.class, args);
+	}
+
+}
